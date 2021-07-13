b1b5e940e5147d1cdc66a43a9761a693cc0c12c4
Alex D'Amico <AlexDamico@users.noreply.github.com>
2021-07-13 12:53:29 -0400

    feature(init): strip factory code

---
+++ src/components/AuthPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="auth-page">
+    <div class="container">
+      hi
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class AuthPage extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style scoped lang="scss">
+</style>
