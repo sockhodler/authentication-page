350b2edc200ffe7bb3e9cdefc3d4a90733ed1c63
Alex D'Amico <AlexDamico@users.noreply.github.com>
2021-07-21 14:56:47 -0400

    feature(auth): add basic close function

--- deliverable/assets/js/functions.js
+++ deliverable/assets/js/functions.js
@@ -3,3 +3,8 @@ var desc = document.querySelector('.description');
 desc.onclick = function() {
   desc.classList.toggle('hide');
 }
+
+var close = document.querySelector('.btn-close');
+close.onclick = function() {
+  document.querySelector('.auth-page-wrapper').style.display = "none";
+}
